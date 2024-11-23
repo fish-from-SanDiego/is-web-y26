3e70b06466eb57c6de13054e3cf784789a76dae3
Maksim Petrov <petrov.maksim5@mail.ru>
2024-11-23 23:14:01 +0300

    feat: add page load time to article page

---
+++ lab4/resources/js/page-load-time.js
@@ -0,0 +1,25 @@
+(function () {
+    const scriptStartTime = Date.now();
+    let domLoadDelta;
+    let pageLoadDelta;
+    const onDOMLoad = () => {
+        domLoadDelta = (Date.now() - scriptStartTime) / 1000;
+        console.log(`DOM load time is ${domLoadDelta}`);
+    };
+    const onPageLoad = () => {
+        pageLoadDelta = (Date.now() - scriptStartTime) / 1000;
+        console.log(`Page load time is ${pageLoadDelta}`);
+        const sidebarElement = document.querySelector('.aside');
+        if (sidebarElement) {
+            const text = document.createElement('p');
+            text.classList.add('aside__item-text');
+            text.innerHTML = `Кстати, эта страница загрузилась за ${pageLoadDelta} секунд! (а DOM - за ${domLoadDelta})`;
+            const newItem = document.createElement('div');
+            newItem.classList.add('aside__item');
+            newItem.appendChild(text);
+            sidebarElement.appendChild(newItem);
+        }
+    };
+    document.addEventListener('DOMContentLoaded', onDOMLoad);
+    window.addEventListener('load', onPageLoad);
+})();
