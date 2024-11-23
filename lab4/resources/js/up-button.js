dfb7b20f451f27a396308cdeddbbb3b666492e3b
Maksim Petrov <petrov.maksim5@mail.ru>
2024-11-24 02:12:20 +0300

    feat: add up button

---
+++ lab4/resources/js/up-button.js
@@ -0,0 +1,26 @@
+{
+    const buttonElement = document.querySelector('.main__up-button');
+
+    window.addEventListener('scroll', hideUpButtonIfNeeded);
+    buttonElement.addEventListener('click', onUpButtonClick);
+
+    function onUpButtonClick() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+    function hideUpButtonIfNeeded() {
+        const footerElement = document.querySelector('.page-footer');
+        const windowHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        const footerCoords = footerElement.getBoundingClientRect();
+        const buttonElement = document.querySelector('.main__up-button');
+        const buttonClasses = buttonElement.classList;
+        if (!buttonElement) return;
+        if (window.scrollY === 0 || (footerCoords.top - windowHeight) <= 0)
+            buttonClasses.add('main__up-button--hidden');
+        else
+            buttonClasses.remove('main__up-button--hidden');
+    }
+}
